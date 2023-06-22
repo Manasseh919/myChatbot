6b76bbb13fbf829ba0918dd1583b340535d29480
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-06-22 09:50:10 +0000

    Rendering of chat buddy

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import ChatFaceData from "../Services/ChatFaceData";
 
@@ -28,6 +28,34 @@ const HomeScreen = () => {
         style={{ width: 150, height: 150, marginTop: 20 }}
       />
       <Text style={{ marginTop: 30, fontSize: 25 }}>How can I Help You</Text>
+      <View
+        style={{
+          marginTop: 20,
+          backgroundColor: "#f5f5f5",
+          alignItems: "center",
+          height: 110,
+          padding: 10,
+          borderRadius: 10,
+        }}
+      >
+        <FlatList
+          data={chatFaceData}
+          horizontal={true}
+          renderItem={({ item }) =>
+            selectedChatFaceData.id != item.id && (
+              <View style={{ margin: 15 }}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={{ width: 40, height: 40 }}
+                />
+              </View>
+            )
+          }
+        />
+        <Text style={{ marginTop: 5, fontSize: 17, color: "#b0b0b0" }}>
+          Choose Your Fav ChatBuddy
+        </Text>
+      </View>
     </View>
   );
 };
